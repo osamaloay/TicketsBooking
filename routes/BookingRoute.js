0879a08e7a819730138b2dfc20542a7118441572
kevorkian <kevorkianmano124@gmail.com>
2025-04-16 18:00:18 +0200

    mano 2nd commit renaming for merging

---
+++ routes/BookingRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const { protect, authorize } = require('../Middleware/authMiddleware');
+const { createBooking,getBookingById,cancelBooking} = require('../Controllers/BookingController');
+
+
+router.post('/bookings', protect, authorize('Standard User'), createBooking);//create booking
+router.get('/bookings/:id', protect, authorize('Standard User'), getBookingById);//get booking by id
+router.delete('/bookings/:id', protect, authorize('Standard User'), cancelBooking);//delete booking
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const eventRoutes = require('./routes/eventRoutes');
-const userRoutes = require('./routes/userRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
+const authRoutes = require('./Routes/AuthenticateRoute');
+const eventRoutes = require('./Routes/EventRoute');
+const userRoutes = require('./Routes/UserRoute');
+const bookingRoutes = require('./Routes/BookingRoute');
 
 dotenv.config();
 
@@ -22,7 +22,7 @@ app.use('/api/v1', eventRoutes);
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', bookingRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
